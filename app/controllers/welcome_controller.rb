c3b6f785a3365bef8b64caecd426910a84dd764d
Steve Schofield <stevebschofield@gmail.com>
2014-01-08 11:11:02 +0000

    change to welcome controller inheritance from base

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,3 @@
-class WelcomeController
+class WelcomeController < ActionController::Base
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SecondApp::Application.routes.draw do
 
-  root "welcome#index"
+  root to: "welcome#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
